65c58f9e0e5b24bc08f794c207199cdbdb2247ac
Joinklom <caredda1998@gmail.com>
2026-07-19 16:39:49 +0200

    touching squares on grid produces a result

--- script.js
+++ script.js
@@ -4,17 +4,39 @@
 const body = document.querySelector("body");
 const container = document.querySelector("#container");
 
-//  IIFE function for gameboard
+
+const gameboard = {};
+const players = {
+    playerTurn : 2,
+};
+
+//IIFE function for gameboard that makes the grid and assign to the gameboard object his squares keys
 (function Gameboard(){
-    const gameboard = [];
     for (let i=0; i<9; i++){
         let squareElement = "square"+i;
-        gameboard.push(squareElement);
+        gameboard[squareElement] = "";
         const gameboardDiv = document.createElement("div");
         gameboardDiv.id = squareElement;
-        gameboardDiv.className = "square"
-        container.append(gameboardDiv)
+        gameboardDiv.className = "square";
+        container.append(gameboardDiv);
     }
-    console.log(gameboard)
-    return gameboard
-})();
+    //console.log(gameboard);
+    return gameboard;
+})()
+
+
+//Functionality of squares, gameboard squares are going to function like buttons
+const squaresArray = document.querySelectorAll(".square");
+for (let square of squaresArray){
+    if (players.playerTurn === 1){
+        square.addEventListener("click", () =>{
+            square.textContent = "X";    
+        })
+    }
+    else {
+        square.addEventListener("click", () =>{
+            square.textContent = "O";    
+        })
+    }
+    
+}
