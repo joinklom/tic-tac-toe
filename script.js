5731671f99c4f6b228a55d8f48a26368599427cb
Joinklom <caredda1998@gmail.com>
2026-07-19 18:45:29 +0200

    .

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const players = {
 
 //IIFE function for gameboard that makes the grid and assign to the gameboard object his squares keys
 (function Gameboard(){
-    for (let i=0; i<9; i++){
+    for (let i=1; i<10; i++){
         let squareElement = "square"+i;
         gameboard[squareElement] = "";
         const gameboardDiv = document.createElement("div");
@@ -20,7 +20,7 @@ const players = {
         gameboardDiv.className = "square";
         container.append(gameboardDiv);
     }
-    //console.log(gameboard);
+    console.log(gameboard);
     return gameboard;
 })()
 
@@ -30,6 +30,9 @@ const squaresArray = document.querySelectorAll(".square");
 for (let square of squaresArray){
     square.addEventListener("click", () =>{
         writeSquare(square)
+        gameboard[square.id] = square.textContent;
+        console.log(gameboard);
+        checkWin(square.textContent);
     })
 }
 
@@ -45,4 +48,41 @@ function writeSquare(square){
         return players.playerTurn = 1;
     }
 }
+
+
+
+//This checks for what is the last letter that has been wrote, and then look for the victoryConditions 
+function checkWin(letter) {
+    Object.keys(gameboard).forEach(key => {
+        if (gameboard[key] === letter){   
+            console.log(`is ${letter}`)
+        }
+    })
+} 
+
+// All valid combinations of equally written squares necessary for winning a game 
+const victoryConditions = {
+    vic1 : [1,5,9],
+    vic2 : [3-5-7],
+    vic3 : [1-4-7],
+    vic4 : [2-5-8],
+    vic5 : [3-6-9],
+    vic6 : [1-2-3],
+    vic7 : [4-5-6],
+    vic8 : [7-8-9]
+};
+
+
+
+// Dunno how to use this actually
+let victory = false;
+function Victory(sq1, sq2, sq3) {
+    if (sq1 === sq2 && sq1 === sq3) {
+        victory = true;
+        return;
+    } 
+}
+
+
+
 
