86d38ba1215f77483dc6ab7483b73531b1e5ac6c
Joinklom <caredda1998@gmail.com>
2026-07-20 12:07:05 +0200

    safe save here

--- script.js
+++ script.js
@@ -7,8 +7,39 @@ const container = document.querySelector("#container");
 
 const gameboard = {};
 const players = {
+    playerOne : "X",
+    playerTwo : "O",
     playerTurn : 1,
 };
+let turn = 1
+
+/*
+// FACTORY FUNCTION PER CREARE OGGETTI, MA NON SO A CHE DOVREBBE SERVIRMI?
+function player(letter, turn, score){
+    return {
+        letter: letter,
+        turn: turn,
+        score: score
+    };
+}
+
+let playerOne = player("X",1,0);
+console.log(playerTwo);
+
+*/
+console.log(players.playerOne);
+// An object with All valid combinations of equally written squares necessary for winning a game 
+const victoryConditions = {
+    vic1 : [1,5,9],
+    vic2 : [3,5,7],
+    vic3 : [1,4,7],
+    vic4 : [2,5,8],
+    vic5 : [3,6,9],
+    vic6 : [1,2,3],
+    vic7 : [4,5,6],
+    vic8 : [7,8,9]
+};
+console.table(victoryConditions);  
 
 //IIFE function for gameboard that makes the grid and assign to the gameboard object his squares keys
 (function Gameboard(){
@@ -49,8 +80,6 @@ function writeSquare(square){
     }
 }
 
-
-
 //This checks for what is the last letter that has been wrote, and then look for the victoryConditions 
 function checkWin(letter) {
     Object.keys(gameboard).forEach(key => {
@@ -59,19 +88,6 @@ function checkWin(letter) {
         }
     })
 } 
-
-// All valid combinations of equally written squares necessary for winning a game 
-const victoryConditions = {
-    vic1 : [1,5,9],
-    vic2 : [3-5-7],
-    vic3 : [1-4-7],
-    vic4 : [2-5-8],
-    vic5 : [3-6-9],
-    vic6 : [1-2-3],
-    vic7 : [4-5-6],
-    vic8 : [7-8-9]
-};
-
 
 
 // Dunno how to use this actually
