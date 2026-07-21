e1623ec7c3bcead98f2d62a55828484264cb7b8e
Joinklom <caredda1998@gmail.com>
2026-07-21 19:53:41 +0200

    project concluded

--- script.js
+++ script.js
@@ -17,8 +17,19 @@ function player(name, letter, turn, combo, score){
         score: score
     };
 }
-const playerOne = player("name1","X",1,[],0);
-const playerTwo = player("name2","O",2,[],0);
+let chosenName = "" 
+const buttonSubmit = document.querySelector("#inputSubmit");
+buttonSubmit.addEventListener("click", () => {
+    const inputName1 = document.querySelector("#playerOne");
+    buttonSubmit.style.display = "none";
+    chosenName = document.querySelector("#playerOne").value;
+    inputName1.style.display = "none";
+    playerOne.name = chosenName;
+    return;
+}) 
+
+const playerOne = player("Player","X",1,[],0);
+const playerTwo = player("Opponent","O",2,[],0);
 
 // An object with All valid combinations of equally written squares necessary for winning a game 
 const victoryConditions = {
@@ -33,7 +44,7 @@ const victoryConditions = {
 };
 
 //IIFE function for gameboard that makes the grid and assign to the gameboard object his squares keys
-function Gameboard(){
+(function () {
     for (let i=1; i<10; i++){
         let squareElement = "square"+i;
         gameboard[squareElement] = "";
@@ -42,11 +53,8 @@ function Gameboard(){
         gameboardDiv.className = "square";
         container.append(gameboardDiv);
     }
-    console.table(gameboard);
     return gameboard;
-}
-
-
+})();
 
 ////////////////// *******************************************************
 
@@ -66,24 +74,27 @@ function writeSquare(square){
 
 // This below compares the combo score of the players object with the arrays inside the victory conditions object
 function checkVictory(player) {
+    if (victory === true) {
+        return;
+    }
     for (let key of Object.keys(victoryConditions)) {
         if (player.combo.includes(`square${victoryConditions[key][0]}`) &&
             player.combo.includes(`square${victoryConditions[key][1]}`) &&
             player.combo.includes(`square${victoryConditions[key][2]}`)) {
             victory = true;
             winner = player;
+            winner.score ++;
             console.log(winner.name);
             return;
         }
     } 
 }
 
-
+const buttons = document.querySelectorAll("button");
 function showEndButtons() {
-    const buttons = document.querySelectorAll("button");
     for (let i=0; i<buttons.length; i++) {
         if (victory === true) {
-            buttons[i].style.display = "block";
+            buttons[i].style.visibility = "visible";
         } 
     } 
 }
@@ -94,7 +105,6 @@ function showEndButtons() {
 function checkState(letter) {
     Object.keys(gameboard).forEach(key => {
         if (gameboard[key] === letter){   
-            console.log(`${key} is ${letter}`)
             if (letter === playerOne.letter) {
                 if (playerOne.combo.includes(key)) {
                     return lastPlayerTurn = playerOne.name;
@@ -104,8 +114,6 @@ function checkState(letter) {
                     return lastPlayerTurn = playerTwo.name;
                 } playerTwo.combo.push(key);                
             }
-            console.log(playerOne)
-            console.log(playerTwo)
         }
     })
 } 
@@ -116,18 +124,11 @@ function checkState(letter) {
 // and calls the function that makes the content word going on and off 
 function endGame(winner) {
     if (victory === true) {
-        const children = document.querySelectorAll(".square");
-        const sidebar = document.querySelector("#sidebar");
-        for (let i=0; i<children.length; i++) {
-            children[i].style = "display: none";
-        } 
         showEndButtons()
-        winner.score ++;
-        content.innerHTML = `${playerOne.name} is at ${playerOne.score} victories<br><br><br>
-            ${playerTwo.name} is at ${playerTwo.score} victories`;
-
-        flicker(content);
+        //flicker(content);
         showWinner(winner);
+        console.table(playerOne);
+        console.table(playerTwo);
     }
 }
 
@@ -136,8 +137,6 @@ let lastPlayerTurn = ""
 let winner = "" 
 let victory = false;
 function game (){
-    Gameboard();
-    reset();
     const squaresArray = document.querySelectorAll(".square");
     for (let square of squaresArray){
         square.addEventListener("click", () =>{
@@ -154,18 +153,23 @@ function game (){
 
 function flicker(element){
     function flickerOff () { 
-        element.style.display = "none";
+        element.style.visibility = "hidden";
     }
 
     function flickerOn () { 
-        element.style.display = "block";          
+        element.style.visibility = "visible";          
     }
-    setInterval(flickerOff, 500)
-    setInterval(flickerOn, 1000)
+    const firstInterval  = setInterval(flickerOff, 500)
+    const secondInterval = setInterval(flickerOn, 1000)
+    if (victory === false) {
+        clearInterval(firstInterval)
+        clearInterval(secondInterval)
+    }
 }
 // We resets the gameboard Object values, 
 // we reset the text content of the squares dom elements
 // we resets the player Objects combo
+
 function reset () {
     for (let i=1; i<10; i++) {
         let index = `square`+i;
@@ -175,17 +179,30 @@ function reset () {
     for (let square of squares) {
         square.textContent = "";
     }
-    playerOne.combo = []
-    playerTwo.combo = []
+    content.textContent = "";
+    const btnContainer = document.querySelector("#buttonsContainer")
+    btnContainer.style.visibility = "hidden"; 
+    playerOne.combo = [];
+    playerTwo.combo = [];
+    victory = false;
+    winner = "";
+    turn = 1;
 }
 
 function showWinner (winner) { 
-    tail.innerHTML += (`${winner.name} scored a point!`) 
-    content.innerHTML = (`${winner.name} is at ${winner.score} point!`);
+    content.innerHTML = (`${winner.name} scored a point!`) 
+    tail.innerHTML = (`${playerOne.name} is at ${playerOne.score} point<br><br><br>
+        ${playerTwo.name} is at ${playerTwo.score} points`);
 }
 
 
-
+const newGamebtn = document.querySelector("#newGame")
+newGamebtn.addEventListener("click", () =>{
+    reset();
+    console.table(gameboard)
+    console.log(playerOne)
+    console.log(playerTwo)
+})
 
 game();
 
